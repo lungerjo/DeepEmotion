6fb86824397d778c2c96bf0cae10d551b6f2da06
lungerjo <j.lunger@mail.utoronto.ca>
2024-09-21 14:20:28 -0400

    generate_test_data.py

--- data/raw/code/get_data.sh
+++
@@ -1,5 +0,0 @@
-datalad install \
-	src/aligned/src/tnt/sub-*/bold3Tp2/brain{,_mask}.nii.gz \
-	src/aligned/src/tnt/sub-*/bold3Tp2/in_grpbold3Tp2/tmpl2subj_warp.nii.gz \
-	src/aligned/src/tnt/templates/grpbold3Tp2/xfm/mni2tmpl_12dof.mat \
-	src/aligned/sub-*/in_bold3Tp2/sub-*_task-avmovie_run-*
