093fd3cf2fef23a325c803e88a6f19c399308607
lungerjo <j.lunger@mail.utoronto.ca>
2024-10-19 18:48:18 -0400

    get nii shape

--- src/scripts/get_nii_shape.py
+++ src/scripts/get_nii_shape.py
@@ -3,14 +3,10 @@ import sys
 from pathlib import Path
 import os
 
-file_path = Path("/Users/joshualunger/DeepEmotion/data/raw/sub-14/ses-forrestgump/func/sub-14_ses-forrestgump_task-forrestgump_rec-dico7Tad2grpbold7TadBrainMaskNLBrainMask_run-02_bold.nii.gzz")
-print(file_path.exists())  # Should print True if the path is correct
-
-"""
 PROJECT_ROOT = str(Path(__file__).resolve().parent.parent.parent)
 os.environ['PROJECT_ROOT'] = PROJECT_ROOT
 print(PROJECT_ROOT)
-file_path = str(Path(PROJECT_ROOT) / "data" / "raw" / "sub-04" / "ses-forrestgump" / "func" / "sub-04_ses-forrestgump_task-forrestgump_rec-dico7Tad2grpbold7TadNL_run-04_bold.nii.gz")
+file_path = str(Path(PROJECT_ROOT) / "data" / "raw" / "derivatives/non-linear_anatomical_alignment/sub-03/ses-forrestgump/func/sub-03_ses-forrestgump_task-forrestgump_rec-dico7Tad2grpbold7TadNL_run-03_bold.nii.gz")
 def get_nii_shape():
 
     img = nib.load(file_path)
@@ -21,4 +17,3 @@ def get_nii_shape():
 
 if __name__ == "__main__":
     get_nii_shape()
-"""
